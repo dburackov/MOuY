2365c970af097aeb5c560297b6bc5bd479b06122
dburackov <dmburackov@yandex.by>
2023-03-18 22:44:30 +0300

    main phase of simplex method optimization

--- simplex_method.py
+++ simplex_method.py
@@ -1,4 +1,5 @@
 import numpy as np
+import mtrinv
 
 
 def get_base_matrix(a_matrix, b_base_plan):
@@ -12,41 +13,26 @@ def get_base_matrix(a_matrix, b_base_plan):
     
     return result
 
-def simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter = 1):
-    #1 step
+def simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter = 1, a_base_matrix_inv = []):
     if iter == 1:
-        pass
-    else:
-        pass
-    a_base_matrix = get_base_matrix(a_matrix, b_base_plan)
-    a_base_matrix_inv = np.linalg.inv(a_base_matrix) 
-
-    #2 step
+        a_base_matrix_inv = np.linalg.inv(get_base_matrix(a_matrix, b_base_plan)) 
     c_base_vector = []
     for i in b_base_plan:
         c_base_vector.append(c_vector[i])
     #print(c_base_vector, 'cB')
-    #3 step
     u_vector = (np.matrix(c_base_vector) * a_base_matrix_inv).A1
     #print(u_vector, 'potential vector')
-    #4 step
-    #marks_vector = a_matrix.dot(u_vector) - c_vector
-    #marks_vector = np.squeeze(np.asarray(np.matrix(u_vector) * a_matrix)) - c_vector
     marks_vector = (np.matrix(u_vector) * a_matrix).A1 - c_vector
     #print(marks_vector, 'marks vector')
-    #5 step
     if marks_vector.dot(marks_vector < 0) == 0:
         return x_base_plan
-    #6 step
     j0 = (marks_vector < 0).tolist().index(True) 
     #print(j0, 'negative component in marks vector')
-    #7 step
     a_j0 = []
     for i in range(len(a_matrix)):
         a_j0.append(a_matrix[i][j0])
     z_vector = a_base_matrix_inv.dot(a_j0)
     #print(z_vector, 'z vector')
-    #8 step
     q_vector = []
     for i in range(len(z_vector)):
         if z_vector[i] > 0:
@@ -54,17 +40,12 @@ def simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter = 1):
         else:
             q_vector.append(np.Infinity)
     #print(q_vector, 'q vector')
-    #9 step
     q0 = min(q_vector)
-    #10 step
     if q0 == np.Infinity: 
-        Exception('целевой функционал задачи не ограничен сверху на множестве допустимых планов')
-    #11 step
+        raise Exception('целевой функционал задачи не ограничен сверху на множестве допустимых планов')
     k = q_vector.index(q0)
     j_k = b_base_plan[k]
-    #12 step
     b_base_plan[k] = j0
-    #13 step
     x_base_plan[j0] = q0
 
     for i in range(len(b_base_plan)):
@@ -74,7 +55,10 @@ def simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter = 1):
     x_base_plan[j_k] = 0
     #print(x_base_plan)
     #print(b_base_plan)
-    return simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter + 1)
+
+    tmp = mtrinv.get_inverse_matrix(a_base_matrix_inv, np.array(a_matrix)[:, j0], k)
+
+    return simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter + 1, np.array(tmp))
 
 
 if __name__ == '__main__':
