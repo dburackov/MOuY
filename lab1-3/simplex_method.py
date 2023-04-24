95a5ba86b3217aa2e139e497628d7f150c6ae9fc
dburackov <dmburackov@yandex.by>
2023-04-24 15:25:03 +0300

    lab3

--- lab1-3/simplex_method.py
+++ lab1-3/simplex_method.py
@@ -28,7 +28,7 @@ def simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter = 1, 
     marks_vector = (np.matrix(u_vector) * a_matrix).A1 - c_vector
     #print(marks_vector, 'marks vector')
     if marks_vector.dot(marks_vector < 0) == 0:
-        return x_base_plan
+        return x_base_plan, b_base_plan
     j0 = (marks_vector < 0).tolist().index(True) 
     #print(j0, 'negative component in marks vector')
     a_j0 = []
@@ -65,13 +65,90 @@ def simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter = 1, 
     return simplex_method_main(c_vector, a_matrix, x_base_plan, b_base_plan, iter + 1, np.array(tmp))
 
 
-if __name__ == '__main__':
+def simplex_method_init(c_vec, a_mtx, b_vec):
+    n = len(a_mtx[0])
+    m = len(a_mtx)
+    #1
+    for i in range(m):
+        if b_vec[i] < 0:
+            b *= -1
+            a_mtx[i] = (np.array(a_mtx[i]) * -1).tolist()
+    #2
+    c_2 = [0] * n + [-1] * m
+
+    a_2 = [a_mtx[i].copy() for i in range(m)]
+    id_mtx = np.eye(m)
+    for i in range(m):
+        for j in range(m):
+            a_2[i].append(id_mtx[i][j])
+    #3
+    x_init_plan = [0] * n + b_vec
+    b_init_plan = [(n + i + 1) for i in range(m)]
+    #4
+    x_init_plan, b_init_plan = simplex_method_main(c_2, a_2, x_init_plan, b_init_plan)
+    #5
+    for i in range(m):
+        if x_init_plan[n + i] != 0:
+            raise Exception("задача не совместима")
+    #6
+    x_base_plan = x_init_plan[:n]
+    #7
+    while max(b_init_plan) >= n:
+        #8
+        j_k = max(b_init_plan)
+        k = b_init_plan.index(j_k)
+        #7
+        a_2_inv = np.linalg.inv(get_base_matrix(a_2, b_init_plan)) 
+
+        flag = False
+        for j in range(n):
+            if j not in b_init_plan:
+                l = a_2_inv.dot(np.array(a_2)[:, j])
+                if l[k] != 0:
+                    b_init_plan[k] = j
+                    flag = True
+                    break
+
+        if not flag:  
+            i = j_k - n
+            a_2.pop(i)
+            a_mtx.pop(i)
+            b_vec.pop(i)
+            b_init_plan.remove(j_k)
+
+
+    return x_base_plan, b_init_plan, a_mtx, b_vec
+
+
+def lab3():
+    a = [[1, 1, 1], [2, 2, 2]]
+    c = [1, 0, 0]
+    b = [0, 0]
+
+    print(simplex_method_init(c, a, b))
+
+    a = [[0, -1, 1, 1, 0],
+                [-5, 1, 1, 0, 0],
+                [-8, 1, 2, 0, -1]]
+    c = [-3, 1, 4, 0, 0]
+    b = [1, 2, 3]
+
+    print(simplex_method_init(c, a, b))
+
+
+def lab2():
     c = [1, 1, 0, 0, 0]
     x = [0, 0, 1, 3, 2]
     a = [[-1, 1, 1, 0, 0], [1, 0, 0, 1, 0], [0, 1, 0, 0, 1]]
     b = [3, 4, 5]
     print(simplex_method_main(c, a, x, b))
+    print(x)
+    print(b)
 
+
+if __name__ == '__main__':
+    lab3()
+    
 
 
 
